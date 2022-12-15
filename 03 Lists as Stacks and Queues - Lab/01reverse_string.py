b39e78ec0453c4f4d40fa73a746bafd46c6a0422
Yanko Rashkov <thewhitedead@abv.bg>
2022-12-15 22:40:11 +0200

    Update 01reverse_string.py

--- 03 Lists as Stacks and Queues - Lab/01reverse_string.py
+++ 03 Lists as Stacks and Queues - Lab/01reverse_string.py
@@ -3,9 +3,27 @@ text = list(input())
 
 while len(text) > 0:
     print(text.pop(), end='')
-
-
 ##################################### variant 02 #####################################
+text = list(input())
+
+while text:
+    print(text.pop(), end='')
+
+##################################### variant 03 #####################################
+
+class ReversedString:
+
+    def __init__(self,input_string):
+        self.input_string = list(input_string)
+
+    def printing(self):
+        while self.input_string:
+            print(self.input_string.pop(), end='')
+
+input_text = input()
+output = ReversedString(input_text).printing()
+
+##################################### variant 04 #####################################
 
 class ReversedString:
 
@@ -18,7 +36,6 @@ class ReversedString:
 
 
 output = ReversedString().printing()
-
 
 #################################### TASK CONDITION ############################
 """

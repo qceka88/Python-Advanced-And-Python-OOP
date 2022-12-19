73d53efed30574cda42851c059a0cd27798b8e5e
Yanko Rashkov <thewhitedead@abv.bg>
2022-12-19 16:57:21 +0200

    added problem description

--- 05 Tuples and Sets - Lab/01count_same_values.py
+++ 05 Tuples and Sets - Lab/01count_same_values.py
@@ -40,3 +40,40 @@ output = Main(input_line)
 output.string_to_dict()
 output.message()
 print(output)
+
+
+#################################### TASK CONDITION ############################
+"""
+
+                  1.	Count Same Values
+You will be given numbers separated by a space. Write a program that 
+prints the number of occurrences of each number in the format 
+"{number} - {count} times". The number must be formatted to the 
+first decimal point.
+
+____________________________________________________________________________________________
+Example_01
+
+Input
+-2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3
+
+Output
+-2.5 - 3 times
+4.0 - 2 times
+3.0 - 4 times
+-5.5 - 1 times
+
+____________________________________________________________________________________________
+Example_02
+
+Input
+2 4 4 5 5 2 3 3 4 4 3 3 4 3 5 3 2 5 4 3
+
+Output
+2.0 - 3 times
+4.0 - 6 times
+5.0 - 4 times
+3.0 - 7 times
+
+
+"""

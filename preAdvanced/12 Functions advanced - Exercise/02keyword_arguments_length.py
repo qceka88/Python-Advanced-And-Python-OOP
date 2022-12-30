477038452e8775965a3ed044c95f2db232f564e8
Yanko Rashkov <thewhitedead@abv.bg>
2022-12-30 21:35:34 +0200

    Create 02keyword_arguments_length.py

---
+++ preAdvanced/12 Functions advanced - Exercise/02keyword_arguments_length.py
@@ -0,0 +1,59 @@
+##################################### variant 01 #####################################
+def kwargs_length(**kwargs):
+    return len(kwargs)
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+print(kwargs_length(**dictionary))
+
+
+##################################### variant 02 #####################################
+
+class Main:
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def return_to_main(self):
+        return len(self.kwargs)
+
+
+def kwargs_length(**kwargs):
+    result = Main(**kwargs).return_to_main()
+    return result
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+print(kwargs_length(**dictionary))
+
+#################################### TASK CONDITION ############################
+'''
+                  2.	Keyword Arguments Length
+Create a function called kwargs_length() that can receive some 
+keyword arguments and return their length.
+Submit only your function in the judge system.
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+dictionary = {'name': 'Peter', 'age': 25}
+
+print(kwargs_length(**dictionary))	
+
+Output
+2
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+dictionary = {}
+
+print(kwargs_length(**dictionary))	
+
+Output
+0
+
+
+'''

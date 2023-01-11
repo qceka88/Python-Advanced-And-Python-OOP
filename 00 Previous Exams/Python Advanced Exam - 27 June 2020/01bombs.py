80eafb2e9c4f698be0c670e8161bc9ea227677cc
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-11 19:55:59 +0200

    Update 01bombs.py

--- 00 Previous Exams/Python Advanced Exam - 27 June 2020/01bombs.py
+++ 00 Previous Exams/Python Advanced Exam - 27 June 2020/01bombs.py
@@ -115,3 +115,87 @@ output = Bombs(bomb_effects, bomb_casings)
 output.check_combinations()
 output.prepare_result()
 print(output)
+
+
+#################################### TASK CONDITION ############################
+'''
+                       01Bombs
+
+Ezio is still learning how to make bombs. With their help, he will save civilization. 
+We should help Ezio to make his perfect bombs.
+
+You will be given two sequences of integers, representing bomb effects and bomb casings.
+You need to start from the first bomb effect and try to mix it with the last bomb casing. If the sum of
+their values is equal to any of the materials in the table below – create the bomb corresponding to the 
+value and remove both bomb materials. Otherwise, just decrease the value of the bomb casing by 5.You need to
+stop combining when you have no more bomb effects or bomb casings, or you successfully filled the bombs pouch.
+Bombs:
+•	Datura Bombs: 40
+•	Cherry Bombs: 60
+•	Smoke Decoy Bombs: 120
+To fill the bomb pouch, Ezio needs three of each of the bomb types.
+Input
+•	On the first line, you will receive the integers representing the bomb effects, separated by ", ".
+•	On the second line, you will receive the integers representing the bomb casings, separated by ", ".
+Output
+•	On the first line, print:
+o	if Ezio succeeded to fulfill the bomb pouch: "Bene! You have successfully filled the bomb pouch!"
+o	if Ezio didn't succeed to fulfill the bomb pouch: "You don't have enough materials to fill the bomb pouch."
+•	On the second line, print all bomb effects left:
+o	If there are no bomb effects: "Bomb Effects: empty"
+o	If there are effects: "Bomb Effects: {bombEffect1}, {bombEffect2}, (…)"
+•	On the third line, print all bomb casings left:
+o	If there are no bomb casings: "Bomb Casings: empty"
+o	If there are casings: "Bomb Casings: {bombCasing1}, {bombCasing2}, (…)"
+•	Then, you need to print all bombs and the count you have of them, ordered alphabetically:
+o	"Cherry Bombs: {count}"
+o	"Datura Bombs: {count}"
+o	"Smoke Decoy Bombs: {count}"
+Constraints
+•	All of the given numbers will be valid integers in the range [0, 120].
+•	There will be no cases with negative material.
+
+_______________________________________________
+Example_01
+
+Input
+5, 25, 25, 115
+5, 15, 25, 35
+
+Output
+You don't have enough materials to fill the bomb pouch.
+Bomb Effects: empty
+Bomb Casings: empty
+Cherry Bombs: 0
+Datura Bombs: 3
+Smoke Decoy Bombs: 1
+
+Example
+1) 5 + 35 = 40 -> Datura Bomb. Remove both.
+2) 25 + 25 = 50 -> can't create bomb. Bomb casing should be decreased with 5 -> 20
+3) 25 + 20 = 45 -> can't create bomb. Bomb casing should be decreased with 5 -> 15
+4) 25 + 15 = 40 -> Datura Bomb. Remove both
+…
+
+_______________________________________________
+Example_02
+
+Input
+30, 40, 5, 55, 50, 100, 110, 35, 40, 35, 100, 80
+20, 25, 20, 5, 20, 20, 70, 5, 35, 0, 10
+
+Output
+Bene! You have successfully filled the bomb pouch!
+Bomb Effects: 100, 80
+Bomb Casings: 20
+Cherry Bombs: 3
+Datura Bombs: 4
+Smoke Decoy Bombs: 3
+
+Output
+After creating a bomb with bomb effect 35 and bomb casing 25, have created 3 Cherry bombs, 
+4 Datura bombs, and 3 Smoke Decoy bombs. From all of the bomb types we have 3 bombs,
+so the program ends.
+
+
+'''

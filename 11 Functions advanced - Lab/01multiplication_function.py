abd69629f00492d979f83871baf55f8033425e59
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-24 22:30:32 +0200

    Update 01multiplication_function.py

--- 11 Functions advanced - Lab/01multiplication_function.py
+++ 11 Functions advanced - Lab/01multiplication_function.py
@@ -16,7 +16,30 @@ def multiply(*args):
     output = Multiply(*args).multiply_numbers()
     return output
 
-
+# This part below is part from automatic test code from Judge system in SoftUni
 print(multiply(1, 4, 5))
 print(multiply(4, 5, 6, 1, 3))
 print(multiply(2, 0, 1000, 5000))
+
+
+#################################### TASK CONDITION ############################
+'''
+                1.	Multiplication Function
+Write a function called multiply that can receive any quantity of numbers 
+(integers) as different parameters and returns the result of the 
+multiplication of all of them. Submit only your function in the Judge system.
+
+_______________________________________________
+Examples
+
+Test Code	(no input data in this task)
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))	
+
+Output
+20
+360
+0
+
+'''

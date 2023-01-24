51ed2bbd5f47b66b3d7506716afff874f1c7ea5f
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-24 23:07:02 +0200

    Update 04rectangle.py

--- 11 Functions advanced - Lab/04rectangle.py
+++ 11 Functions advanced - Lab/04rectangle.py
@@ -27,6 +27,43 @@ def rectangle(*args):
     output = Rectangle(*args).main_meth()
     return output
 
-
+# This part below is part from automatic test code from Judge system in SoftUni
 print(rectangle(2, 10))
 print(rectangle('2', 10))
+
+
+#################################### TASK CONDITION ############################
+'''
+                           4.	Rectangle
+Create a function called rectangle(). It must have two parameters - length and width. 
+First, you need to check if the given arguments are integers:
+•	If one/ both of them is/ are NOT an integer/s, return the string "Enter valid values!"
+Create two inner functions:
+•	area() - returns the area of the rectangle with the given length and width
+•	perimeter() - returns the perimeter of the rectangle with the given length and width
+In the end, the rectangle function should return a string containing the 
+area and the perimeter of a rectangle in the following format:
+"Rectangle area: {ract_area}
+Rectangle perimeter: {rect_perim}"
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+print(rectangle(2, 10))	
+
+Output
+Rectangle area: 20
+Rectangle perimeter: 24
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+print(rectangle('2', 10))	
+
+Output
+"Enter valid values!"
+
+
+'''

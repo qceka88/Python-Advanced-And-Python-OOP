79abab3beea0715aacfe028fca5d43669a21b974
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-29 00:43:53 +0200

    Create 03employee.py

---
+++ Python-OOP/preOOP/04 First Steps in OOP - Lab/03employee.py
@@ -0,0 +1,47 @@
+class Employee:
+
+    def __init__(self, *args):
+        self.id, self.first_name, self.last_name, self.salary = args
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def raise_salary(self, raise_salary):
+        self.salary += raise_salary
+        return self.salary
+
+    def get_annual_salary(self):
+        return self.salary * 12
+
+
+# Part below is part from automatic judge system from SoftUni
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
+
+
+#################################### TASK CONDITION ############################
+'''
+3.	Employee
+Create class Employee. Upon initialization, it should receive id (number), first_name (string), last_name (string) and salary (number). Create 3 instance methods:
+-	get_full_name() - returns "{first_name} {last_name}"
+-	get_annual_salary() - returns the total salary for 12 months
+-	raise_salary(amount) - increases the salary by the given amount and returns the new salary
+
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
+
+Output
+John Smith
+1500
+18000
+
+'''

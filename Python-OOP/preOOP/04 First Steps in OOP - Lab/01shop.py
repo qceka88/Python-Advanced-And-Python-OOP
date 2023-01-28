3f10892ad9e1ee99e25a8da5fb751bb2f74d5f63
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-29 00:19:42 +0200

    update

--- Python-OOP/preOOP/04 First Steps in OOP - Lab/01shop.py
+++ Python-OOP/preOOP/04 First Steps in OOP - Lab/01shop.py
@@ -7,6 +7,25 @@ class Shop:
         number_of_items = len(self.items)
         return number_of_items
 
+# Part below is part from automatic judge system from SoftUni
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
+
+#################################### TASK CONDITION ############################
+'''
+                 1.	Shop
+Create a class called Shop. Upon initialization it should receive a name (string) and items (list). 
+Create a method called get_items_count() which should return the number of items in the store.
+
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
 
 shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
 print(shop.get_items_count())
+
+Output
+3
+
+'''

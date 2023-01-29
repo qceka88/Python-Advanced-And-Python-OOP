3bb42ee2b819946c6d343ee5f3fbe44f4932203b
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-29 22:09:16 +0200

    Update 01vehicle.py

--- Python-OOP/preOOP/05 Classes and Objects - Lab/01vehicle.py
+++ Python-OOP/preOOP/05 Classes and Objects - Lab/01vehicle.py
@@ -11,3 +11,31 @@ print(car.mileage)
 print(car.gadgets)
 car.gadgets.append('Hudly Wireless')
 print(car.gadgets)
+
+
+
+#################################### TASK CONDITION ############################
+'''
+                              1.	Vehicle
+Create a class called Vehicle. Upon initialization it should receive max_speed
+(number, optional; 150 by default) and mileage (number). 
+Create an instance variable called gadgets – empty list by default.
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)
+
+Output
+150
+20
+[]
+['Hudly Wireless']
+
+'''

16699e7075f3e234b82aab715ec05eace0e330db
Yanko Rashkov <thewhitedead@abv.bg>
2023-01-29 23:09:05 +0200

    Create 04glass.py

---
+++ Python-OOP/preOOP/05 Classes and Objects - Lab/04glass.py
@@ -0,0 +1,60 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, milliliters):
+        if self.content + milliliters <= Glass.capacity:
+            self.content += milliliters
+            return f'Glass filled with {milliliters} ml'
+        return f'Cannot add {milliliters} ml'
+
+    def empty(self):
+        self.content = 0
+        return 'Glass is now empty'
+
+    def info(self):
+        space_left = Glass.capacity - self.content
+        return f'{space_left} ml left'
+
+
+# Part below is part from automatic judge system from SoftUni
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
+
+#################################### TASK CONDITION ############################
+'''
+                  4.	Glass
+Create a class called Glass. Upon initialization it will not receive any parameters,
+you must create however an instance attribute called content which should be equal to 0.
+You should also create a class attribute called capacity which should be 250 ml.
+Create 3 instance methods:
+-	fill(ml) - fill the glass with the given milliliters if there is enough space 
+in it and return "Glass filled with {ml} ml", otherwise return "Cannot add {ml} ml"
+-	empty() - empty the glass and return "Glass is now empty" 
+-	info() - returns info about the glass in the format "{space_left} ml left"
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
+
+Output
+Glass filled with 100 ml
+Cannot add 200 ml
+Glass is now empty
+Glass filled with 200 ml
+50 ml left
+
+'''

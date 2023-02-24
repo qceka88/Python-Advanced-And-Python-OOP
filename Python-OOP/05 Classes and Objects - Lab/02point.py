896b644974740e00e4d401aa92b9215ee34bba7e
Yanko Rashkov <thewhitedead@abv.bg>
2023-02-24 22:51:51 +0200

    Update 02point.py

--- Python-OOP/05 Classes and Objects - Lab/02point.py
+++ Python-OOP/05 Classes and Objects - Lab/02point.py
@@ -11,7 +11,7 @@ class Point:
         self.y = new_y
 
     def __str__(self):
-        return f"The point has coordinates ({self.x},{self.y}) "
+        return f"The point has coordinates ({self.x},{self.y})"
 
 
 # Part below is part from automatic judge system from SoftUni

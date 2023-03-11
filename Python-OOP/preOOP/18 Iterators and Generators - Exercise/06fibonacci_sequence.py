645f91926f7b9bc4710eac9feabd6ab87b438144
Yanko Rashkov <thewhitedead@abv.bg>
2023-03-11 17:58:44 +0200

    Create 06fibonacci_sequence.py

---
+++ Python-OOP/preOOP/18 Iterators and Generators - Exercise/06fibonacci_sequence.py
@@ -0,0 +1,62 @@
+def fibonacci():
+    num1, num2 = 0, 1
+    while True:
+        yield num1
+
+        num1, num2 = num2, num1 + num2
+
+
+# Part below is part from automatic judge system from SoftUni
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
+generator = fibonacci()
+for i in range(1):
+    print(next(generator))
+
+#################################### TASK CONDITION ############################
+'''
+           6.	Fibonacci Generator
+Create a generator function called fibonacci() that generates the Fibonacci 
+numbers infinitely. The first two numbers in the sequence are always 0 and 1. 
+Each following Fibonacci number is created by the sum of the current number with the previous one.
+Note: Submit only the function in the judge system
+
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
+
+Output
+
+0
+1
+1
+2
+3
+
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+
+
+generator = fibonacci()
+for i in range(1):
+    print(next(generator))
+    
+Output
+    
+0
+
+'''

3b9d3cd792bf5f98a330af140ad043f2f4740348
Yanko Rashkov <thewhitedead@abv.bg>
2023-03-12 18:03:40 +0200

    Create 01number_increment.py

---
+++ Python-OOP/preOOP/19 Decorators - Lab/01number_increment.py
@@ -0,0 +1,39 @@
+def number_increment(numbers):
+    def increase():
+        return [x + 1 for x in numbers]
+
+    return increase()
+
+
+print(number_increment([1, 2, 3]))
+
+#################################### TASK CONDITION ############################
+'''
+                  1.	Number Increment
+Having the following code:
+'''
+
+
+def number_increment(numbers):
+    def increase():
+        # TODO: Implement
+        ...
+
+    return increase()
+
+
+'''
+Complete the code, so it works as expected.
+
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+
+
+print(number_increment([1, 2, 3]))
+
+Output
+[2, 3, 4]
+
+'''

c3d23f91a32cfcc7bf9e388f3ce7f83e3856e5ee
Yanko Rashkov <thewhitedead@abv.bg>
2023-03-12 21:36:14 +0200

    Create 04multiply.py

---
+++ Python-OOP/preOOP/19 Decorators - Lab/04multiply.py
@@ -0,0 +1,80 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(number):
+            return times * function(number)
+
+        return wrapper
+
+    return decorator
+
+
+# Part below is part from automatic judge system from SoftUni
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(3))
+
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
+
+#################################### TASK CONDITION ############################
+'''
+4.	Multiply
+Having the following code:
+
+
+'''
+
+
+def multiply(times):
+    def decorator(function):
+        # TODO: Implement
+        ...
+
+    return decorator
+
+
+'''
+Complete the code, so it works as expected.
+
+_______________________________________________
+Example_01
+
+Test Code	(no input data in this task)
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+print(add_ten(3))
+
+
+Output
+
+39
+
+_______________________________________________
+Example_02
+
+Test Code	(no input data in this task)
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+print(add_ten(6))
+
+Output 
+
+
+80
+
+
+'''

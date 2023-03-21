91b80e06ab021f782e67ffd72691301b749e276e
Yanko Rashkov <thewhitedead@abv.bg>
2023-03-21 22:42:23 +0200

    Create 01number_increment.py

---
+++ Python-OOP/19 Decorators - Lab/01number_increment.py
@@ -0,0 +1,40 @@
+def number_increment(numbers):
+    def increase():
+        return [i + 1 for i in numbers]
+
+    return increase()
+
+# Part below is part from automatic judge system from SoftUni
+print(number_increment([1, 2, 3]))
+
+
+#################################### TASK CONDITION ############################
+'''
+                  1.	Number Increment
+Having the following code:
+'''
+
+
+def number_increment(numbers):
+    def increase():
+        # TODO: Implement
+        ...
+
+    return increase()
+
+
+'''
+Complete the code, so it works as expected.
+
+_______________________________________________
+Example
+
+Test Code	(no input data in this task)
+
+
+print(number_increment([1, 2, 3]))
+
+Output
+[2, 3, 4]
+
+'''
